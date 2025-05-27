4cd7142a685c7a75c1ec20665611d9babb7e4643
sensnyaha <mikhail.makarov@sms-a.ru>
2025-05-27 16:23:24 +0400

    добавление деплой ci/cd процесса

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+if docker ps -f name=redis | grep redis > /dev/null; then
+    echo "Контейнер redis запущен"
+else
+    echo "Контейнер redis не запущен"
+    docker run -d --name redis-container -p 6379:6379 redis
+fi
+
+systemctl restart zulip_messages
