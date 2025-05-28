1f6a7bc073eec2f8d28f0f0dad2aec166d86a74e
sensnyaha <mikhail.makarov@sms-a.ru>
2025-05-28 08:06:01 +0400

    добавлена обработка режима разбора сообщения в зависимости от ответа сервера телеграма на сообщение с заданным режимом разметки

--- helpers/app/processing/processUnreadMessages.js
+++ helpers/app/processing/processUnreadMessages.js
@@ -4,13 +4,12 @@ const createNewTelegramNotification = require("../../sqlite/creatings/createNewT
 const getUserFastReactions = require("../../sqlite/gettings/getUserFastReactions");
 const formatTimeLocalized = require("../others/formatTime");
 const convertToEmoji = require("./convertToEmoji");
-const escapeMarkdownV2 = require("../others/escapeMarkdownV2");
 
 
-const processUnreadMessages = (db, bot, messages, user) => {
+const processUnreadMessages = async (db, bot, messages, user) => {
     const userFastReactions = getUserFastReactions(db, user.id);
 
-    messages.forEach(msg => {
+    for (const msg of messages) {
         let choppedMsg = {
             id: msg.id,
             content: msg.content,
@@ -23,30 +22,44 @@ const processUnreadMessages = (db, bot, messages, user) => {
 
         let telegramMessageContent;
         if (choppedMsg.type === "private") {
-            telegramMessageContent = `${choppedMsg.sender_full_name} sent you in ${formatTimeLocalized(choppedMsg.timestamp * 1000)}\n\n${choppedMsg.content}`;
+            telegramMessageContent = `*${choppedMsg.sender_full_name} sent you in ${formatTimeLocalized(choppedMsg.timestamp * 1000)}*\n\n${choppedMsg.content}`;
         } else if (choppedMsg.type === "stream") {
-            telegramMessageContent = `${choppedMsg.sender_full_name} sent in ${choppedMsg.display_recipient} in ${formatTimeLocalized(choppedMsg.timestamp * 1000)}\n\n${choppedMsg.content}`;
+            telegramMessageContent = `*${choppedMsg.sender_full_name} sent in ${choppedMsg.display_recipient} in ${formatTimeLocalized(choppedMsg.timestamp * 1000)}*\n\n${choppedMsg.content}`;
         } else {
             telegramMessageContent = JSON.stringify(choppedMsg);
         }
 
-
-
         if ((Date.now() / 1000 - msg.timestamp) >= user.notify_timeout_sec) {
             const notification = getTelegramNotification(db, user.id, msg.id);
             if (!notification) {
-                bot.telegram.sendMessage(user.id,
-                    convertToEmoji(telegramMessageContent),
-                    {
-                    ...createNotificationReplyKeyboard(db, choppedMsg.id, userFastReactions),
-                    //parse_mode: "Markdown"
-                })
-                    .then(res => {
-                        createNewTelegramNotification(db, user.id, msg.id, res.message_id)
-                    })
+                let res;
+                try {
+                    res = await bot.telegram.sendMessage(user.id,
+                        convertToEmoji(telegramMessageContent),
+                        {
+                            ...createNotificationReplyKeyboard(db, choppedMsg.id, userFastReactions),
+                            parse_mode: "MarkdownV2"
+                        })
+                } catch (error) {
+                    try {
+                        res = await bot.telegram.sendMessage(user.id,
+                            convertToEmoji(telegramMessageContent),
+                            {
+                                ...createNotificationReplyKeyboard(db, choppedMsg.id, userFastReactions),
+                                parse_mode: "Markdown"
+                            })
+                    } catch (error) {
+                        await bot.telegram.sendMessage(user.id,
+                            convertToEmoji(telegramMessageContent),
+                            {
+                                ...createNotificationReplyKeyboard(db, choppedMsg.id, userFastReactions),
+                            })
+                    }
+                }
+                res && createNewTelegramNotification(db, user.id, msg.id, res.message_id)
             }
         }
-    })
+    }
 }
 
 module.exports = processUnreadMessages;
